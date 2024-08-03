271ea781cdb4a523311b742c72f2667ab897f233
Bob Parsons <deadstylebp@gmail.com>
2024-08-02 22:24:54 -0500

    add prod drone and update the types index

--- models/type.model.go
+++ models/type.model.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Type struct {
-	ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name      string    `gorm:"type:varchar(50);not null" json:"name"`
-	Code      string    `gorm:"type:varchar(50);not null;uniqueIndex:idx_category_code" json:"code"`
-	Category  string    `gorm:"type:varchar(50);not null;uniqueIndex:idx_category_code" json:"category"`
-	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
+	ID        int       `gorm:"primaryKey;autoIncrement"              json:"id"`
+	Name      string    `gorm:"type:varchar(50);not null"             json:"name"`
+	Code      string    `gorm:"type:varchar(50);not null;uniqueIndex" json:"code"`
+	Category  string    `gorm:"type:varchar(50);not null;index"       json:"category"`
+	CreatedAt time.Time `gorm:"autoCreateTime"                        json:"createdAt"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"                        json:"updatedAt"`
 }
 
 var types = []Type{
