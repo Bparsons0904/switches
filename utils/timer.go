a4bdeb6214b313bcc1c0043574eb779022354ff5
Bob Parsons <deadstylebp@gmail.com>
2024-08-24 08:42:49 -0500

    update timer

--- utils/timer.go
+++ utils/timer.go
@@ -1,7 +1,7 @@
 package utils
 
 import (
-	"log"
+	"log/slog"
 	"time"
 )
 
@@ -11,7 +11,7 @@ type Timer struct {
 }
 
 func StartTimer(message string) Timer {
-	log.Println("Starting to time: ", message)
+	slog.Info("Starting to time: " + message)
 	return Timer{
 		totalTimer:  time.Now(),
 		sectionTime: time.Now(),
@@ -19,29 +19,10 @@ func StartTimer(message string) Timer {
 }
 
 func (t *Timer) LogTime(message string) {
-	log.Println(message, time.Duration(time.Since(t.sectionTime)).String())
+	slog.Info(message, "Segement Elapsed Time", time.Duration(time.Since(t.sectionTime)).String())
 	t.sectionTime = time.Now()
 }
 
 func (t *Timer) LogTotalTime(message string) {
-	log.Println(message, time.Duration(time.Since(t.totalTimer)).String())
-}
-
-var (
-	totalTimer  time.Time
-	sectionTime time.Time
-)
-
-func StartTimer2() {
-	sectionTime = time.Now()
-	totalTimer = sectionTime
-}
-
-func LogTime2(message string) {
-	log.Println(message, time.Duration(time.Since(sectionTime)).String())
-	sectionTime = time.Now()
-}
-
-func LogTotalTime2(message string) {
-	log.Println(message, time.Duration(time.Since(totalTimer)).String())
+	slog.Info(message, "Total Elapsed Time", time.Duration(time.Since(t.totalTimer)).String())
 }
