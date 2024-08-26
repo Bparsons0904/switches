66a1fc121252548c3e0fe04de55e50172e417d7f
Bob Parsons <deadstylebp@gmail.com>
2024-08-26 07:54:04 -0500

    refactor switch controller to move logic to controller

--- controllers/switches.controller.go
+++ controllers/switches.controller.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"switches/database"
 	"switches/models"
 	"switches/templates/components"
 	"switches/templates/pages"
+	"switches/utils"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -15,16 +17,44 @@ func GetSwitches(c *fiber.Ctx) error {
 }
 
 func GetFeaturedSwitches(c *fiber.Ctx) error {
-	component := components.FeaturedSwitches()
+	timer := utils.StartTimer("getFeaturedSwitches")
+	var switchesFromDB []models.Switch
+	database.DB.
+		Joins("INNER JOIN image_links ON image_links.owner_id = switches.id").
+		Preload("ImageLinks").
+		Limit(4).
+		Order("RANDOM()").
+		Find(&switchesFromDB)
+
+	timer.LogTotalTime()
+
+	var switches []components.SwitchCardProps
+	for _, clickyClack := range switchesFromDB {
+		imageLink := clickyClack.ImageLinks[0].LinkAddress
+		imageAlt := fmt.Sprintf("Image of %s switch", clickyClack.Name)
+		newSwitch := components.SwitchCardProps{
+			ID:          clickyClack.ID.String(),
+			Title:       clickyClack.Name,
+			Description: clickyClack.ShortDescription,
+			ImageSrc:    imageLink,
+			ImageAlt:    imageAlt,
+		}
+
+		switches = append(switches, newSwitch)
+	}
+
+	component := components.FeaturedSwitches(switches)
 	return Render(component)(c)
 }
 
 func GetSwitchModal(c *fiber.Ctx) error {
+	timer := utils.StartTimer("getSwitchModal")
 	switchID := c.Params("switchID")
 
 	var switchModel models.Switch
 	database.DB.First(&switchModel, switchID)
 
+	timer.LogTotalTime()
 	component := components.SwitchModalCard(switchModel)
 	return Render(component)(c)
 }
