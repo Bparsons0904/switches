32cbb131f9e8dfa7aea399f67cab0d012c4b10e1
Bob Parsons <deadstylebp@gmail.com>
2024-09-20 15:27:19 -0500

    updaterating model

--- migrator/migrations/20240911081632_switch_ratings.go
+++ migrator/migrations/20240911081632_switch_ratings.go
@@ -101,15 +101,18 @@ func addRating(tx *gorm.DB) error {
 		ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey" json:"id"`
 	}
 	type Rating struct {
-		ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey"        json:"id"`
-		Rating    int       `gorm:"type:int;not null"                                      json:"rating"`
-		Review    string    `gorm:"type:text"                                              json:"review"`
-		UserID    uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch"                  json:"userId"`
-		User      User      `gorm:"foreignKey:UserID;references:ID"                        json:"user,omitempty"`
-		SwitchID  uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch;index:idx_switch" json:"switchId"`
-		Switch    Switch    `gorm:"foreignKey:SwitchID;references:ID"                      json:"switch,omitempty"`
-		CreatedAt time.Time `gorm:"autoCreateTime"                                         json:"createdAt"`
-		UpdatedAt time.Time `gorm:"autoUpdateTime"                                         json:"updatedAt"`
+		ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey"        json:"id"`
+		Rating         int       `gorm:"type:int;not null"                                      json:"rating"`
+		Review         string    `gorm:"type:text"                                              json:"review"`
+		UserID         uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch"                  json:"userId"`
+		User           User      `gorm:"foreignKey:UserID;references:ID"                        json:"user,omitempty"`
+		SwitchID       uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch;index:idx_switch" json:"switchId"`
+		Switch         Switch    `gorm:"foreignKey:SwitchID;references:ID"                      json:"switch,omitempty"`
+		ToxicityScore  float64   `gorm:"type:float"                                             json:"toxicityScore"`
+		SafeURLScore   float64   `gorm:"type:float"                                             json:"safeRRLScore"`
+		RelavanceScore float64   `gorm:"type:float"                                             json:"relavanceScore"`
+		CreatedAt      time.Time `gorm:"autoCreateTime"                                         json:"createdAt"`
+		UpdatedAt      time.Time `gorm:"autoUpdateTime"                                         json:"updatedAt"`
 	}
 
 	err := tx.Migrator().CreateTable(&Rating{})
@@ -122,14 +125,17 @@ func updatedSwitch(tx *gorm.DB) error {
 		ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey" json:"id"`
 	}
 	type Rating struct {
-		ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey"        json:"id"`
-		Rating    int       `gorm:"type:int;not null"                                      json:"rating"`
-		Review    string    `gorm:"type:text"                                              json:"review"`
-		UserID    uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch"                  json:"userId"`
-		User      User      `gorm:"foreignKey:UserID;references:ID"                        json:"user,omitempty"`
-		SwitchID  uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch;index:idx_switch" json:"switchId"`
-		CreatedAt time.Time `gorm:"autoCreateTime"                                         json:"createdAt"`
-		UpdatedAt time.Time `gorm:"autoUpdateTime"                                         json:"updatedAt"`
+		ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey"        json:"id"`
+		Rating         int       `gorm:"type:int;not null"                                      json:"rating"`
+		Review         string    `gorm:"type:text"                                              json:"review"`
+		UserID         uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch"                  json:"userId"`
+		User           User      `gorm:"foreignKey:UserID;references:ID"                        json:"user,omitempty"`
+		SwitchID       uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch;index:idx_switch" json:"switchId"`
+		ToxicityScore  float64   `gorm:"type:float"                                             json:"toxicityScore"`
+		SafeURLScore   float64   `gorm:"type:float"                                             json:"safeRRLScore"`
+		RelavanceScore float64   `gorm:"type:float"                                             json:"relavanceScore"`
+		CreatedAt      time.Time `gorm:"autoCreateTime"                                         json:"createdAt"`
+		UpdatedAt      time.Time `gorm:"autoUpdateTime"                                         json:"updatedAt"`
 	}
 	type Type struct {
 		ID int `gorm:"primaryKey;autoIncrement" json:"id"`
--- models/rating.model.go
+++ models/rating.model.go
@@ -7,13 +7,16 @@ import (
 )
 
 type Rating struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey"        json:"id"`
-	Rating    int       `gorm:"type:int;not null"                                      json:"rating"`
-	Review    string    `gorm:"type:text"                                              json:"review"`
-	UserID    uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch"                  json:"userId"`
-	User      User      `gorm:"foreignKey:UserID;references:ID"                        json:"user,omitempty"`
-	SwitchID  uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch;index:idx_switch" json:"switchId"`
-	Switch    Switch    `gorm:"foreignKey:SwitchID;references:ID"                      json:"switch,omitempty"`
-	CreatedAt time.Time `gorm:"autoCreateTime"                                         json:"createdAt"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"                                         json:"updatedAt"`
+	ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey"        json:"id"`
+	Rating         int       `gorm:"type:int;not null"                                      json:"rating"`
+	Review         string    `gorm:"type:text"                                              json:"review"`
+	UserID         uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch"                  json:"userId"`
+	User           User      `gorm:"foreignKey:UserID;references:ID"                        json:"user,omitempty"`
+	SwitchID       uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_user_switch;index:idx_switch" json:"switchId"`
+	Switch         Switch    `gorm:"foreignKey:SwitchID;references:ID"                      json:"switch,omitempty"`
+	ToxicityScore  float64   `gorm:"type:float"                                             json:"toxicityScore"`
+	SafeURLScore   float64   `gorm:"type:float"                                             json:"safeRRLScore"`
+	RelavanceScore float64   `gorm:"type:float"                                             json:"relavanceScore"`
+	CreatedAt      time.Time `gorm:"autoCreateTime"                                         json:"createdAt"`
+	UpdatedAt      time.Time `gorm:"autoUpdateTime"                                         json:"updatedAt"`
 }
