b402a3aaf4187ce2278321c6c88c0d2cb9c4e1eb
Bob Parsons <deadstylebp@gmail.com>
2024-10-21 21:38:10 -0500

    Automatically deleted failed files

--- routes/log.routes.go
+++ routes/log.routes.go
@@ -1,28 +1,13 @@
 package routes
 
 import (
+	"switches/controllers"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/rs/zerolog/log"
 )
 
 func LogRoutes(app fiber.Router) {
 	logsApi := app.Group("/logs")
 
-	logsApi.Post("/errors/images", PostImageLog)
-}
-
-func PostImageLog(c *fiber.Ctx) error {
-	type ImageLog struct {
-		Src string `json:"src"`
-	}
-	var imageLog ImageLog
-	err := c.BodyParser(&imageLog)
-	if err != nil {
-		log.Error().Err(err).Msg("Error trying to parse image log body")
-		return c.Status(fiber.StatusBadRequest).
-			JSON(fiber.Map{"status": "error", "message": "Unable to parse src from body", "error": err})
-	}
-
-	log.Error().Str("src", imageLog.Src).Msg("Client Error")
-	return c.JSON(fiber.Map{"status": "success"})
+	logsApi.Post("/errors/images", controllers.PostImageLog)
 }
